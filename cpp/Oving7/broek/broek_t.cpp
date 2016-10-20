bd4b6d01d3f138b7f2bfc04bcf23a510393b8a26
bjornnes <steinerikbjornnes@gmail.com>
2016-10-20 22:43:50 +0200

    Add files via upload

---
+++ cpp/Oving7/broek/broek_t.cpp
@@ -0,0 +1,48 @@
+#include "broek.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void skriv(const string &tekst, const Broek &Broek);
+
+int main() {
+  Broek a(10, 20);
+  Broek b(3, 4);
+  Broek c;
+  c.settBroek(5);
+  Broek d = a / b;
+
+  skriv("a = ", a);
+  skriv("b = ", b);
+  skriv("c = ", c);
+  skriv("d = ", d);
+
+  b += a;
+  ++c;
+  d *= d;
+
+  skriv("b = ", b);
+  skriv("c = ", c);
+  skriv("d = ", d);
+
+  c = a + b - d * a;
+  c = -c;
+
+  skriv("c = ", c);
+
+  if (a + b != c + d)
+    cout << "a + b != c + d" << endl;
+  else
+    cout << " a + b == c + d" << endl;
+  while (b > a)
+    b -= a;
+  skriv("b = ", b);
+
+  return 0;
+}
+
+void skriv(const string &tekst, const Broek &broek) {
+  cout << tekst << broek.finnTeller() << " / ";
+  cout << broek.finnNevner() << endl;
+}
